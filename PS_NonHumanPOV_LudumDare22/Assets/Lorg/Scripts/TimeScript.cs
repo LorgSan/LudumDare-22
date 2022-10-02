643c65811d810ef53eeca774fb1920df3e3ddd30
Lorg <hikariame100@gmail.com>
2022-10-02 18:24:40 -0400

    Bomb timer script done

---
+++ PS_NonHumanPOV_LudumDare22/Assets/Lorg/Scripts/TimeScript.cs
@@ -0,0 +1,81 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeScript : MonoBehaviour
+{
+    [SerializeField] float bombProbability = 0;
+    [SerializeField] float stepIncrease = 0.2f;
+    [SerializeField] int cycleNumbers;
+    [SerializeField] float TimeBetweenCycles = 10f;
+    
+    struct RandomSelection {
+    private int minValue;
+    private int maxValue;
+    public float probability;
+    
+    public RandomSelection(int minValue, int maxValue, float probability) {
+        this.minValue = minValue;
+        this.maxValue = maxValue;
+        this.probability = probability;
+    }
+    
+    public int GetValue() { return Random.Range(minValue, maxValue + 1); }
+    }
+    void Update()
+    {
+        if(Input.GetKeyDown(KeyCode.R))
+        {
+            Debug.Log(Randomizer());
+        }
+    }
+
+    int Randomizer()
+    {
+        int random = GetRandomValue(
+            new RandomSelection(0, 8, 1f - bombProbability),
+            new RandomSelection(9, 9, bombProbability)
+        );
+        return random;
+    }
+    
+    int GetRandomValue(params RandomSelection[] selections) {
+        float rand = Random.value;
+        float currentProb = 0;
+        foreach (var selection in selections) {
+            currentProb += selection.probability;
+            if (rand <= currentProb)
+                return selection.GetValue();
+        }
+    
+        //will happen if the input's probabilities sums to less than 1
+        //throw error here if that's appropriate
+        return -1;
+    }
+
+    void Start()
+    {
+        InvokeRepeating("BombTimer", 0f, TimeBetweenCycles);
+    }
+
+    void BombTimer()
+    {
+        cycleNumbers ++;
+        if (cycleNumbers >= 15)
+        {
+            bombProbability = 1f;
+        }
+
+        int currentRandom = Randomizer();
+        Debug.Log(currentRandom);
+        if (currentRandom == 9)
+        {
+            Debug.Log("bomb went off");
+            CancelInvoke();
+        } else 
+            if (cycleNumbers >= 3)
+            {
+                bombProbability += stepIncrease;
+            }
+    }
+}
